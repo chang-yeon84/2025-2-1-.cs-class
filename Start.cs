87da04465865fb3e3da4ecb1d9ec414a2e1433ee
chang-yeon84 <104890291+chang-yeon84@users.noreply.github.com>
2025-03-12 16:31:20 +0900

    Week1

---
+++ Start.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.Write(a + b);
+            //System = NameSpace, Console = class, WriteLine = Method
+            
+        }
+    }
+}
